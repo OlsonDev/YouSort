b77c6e7e56c50731dd5956ee9520cb5dc8bc500d
Josh Olson <Olson.dev@Gmail.com>
2018-04-18 21:11:26 -0500

    Fix build.

--- core/webpack.base.js
+++ core/webpack.base.js
@@ -11,9 +11,7 @@ module.exports = {
     popup: resolve('./popup'),
     options: resolve('./options'),
     content: resolve('./content'),
-    devtools: resolve('./devtools'),
     background: resolve('./backend'),
-    panel: resolve('./devtools/panel'),
   },
   output: {
     path: path.join(__dirname, '..', 'build'),
@@ -92,7 +90,6 @@ module.exports = {
     htmlPage('home', 'app', ['tab']),
     htmlPage('popup', 'popup', ['popup']),
     htmlPage('panel', 'panel', ['panel']),
-    htmlPage('devtools', 'devtools', ['devtools']),
     htmlPage('options', 'options', ['options']),
     htmlPage('background', 'background', ['background']),
     new CopyWebpackPlugin([{ from: path.join(__dirname, '..', 'static') }]),
